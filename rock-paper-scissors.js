6f8b967662cf433052507afa16ab94eb0cdb934f
Houndoom <therealhoundoom@gmail.com>
2022-06-21 14:21:36 +0800

    Complete final game function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,6 +15,7 @@ function capitaliseFirst(string) {
 }
 
 function playRound(playerSelection, computerSelection) {
+  const options = ['Rock', 'Paper', 'Scissors'];
   if (!playerSelection && !computerSelection) {
     return "Please enter player and computer selections!";
   }
@@ -24,13 +25,30 @@ function playRound(playerSelection, computerSelection) {
   if (!computerSelection) {
     return "Please enter computer selection";
   }
+
   playerSelection = capitaliseFirst(playerSelection);
   computerSelection = capitaliseFirst(computerSelection);
+
+  if (!options.includes(playerSelection)) {
+    return "Please enter a valid player selection!"
+  }
+
+  if (!options.includes(computerSelection)) {
+    return "Please enter a valid computer selection!"
+  }
+  
   if (playerSelection === computerSelection) {
     return "It's a draw!";
   } else if (playerSelection === 'Rock' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Scissors' || playerSelection === 'Scissors' && computerSelection === 'Rock') {
     return "You lose! " + computerSelection + " beats " + playerSelection;
   } else {
     return "You win! " + playerSelection + " beats " + computerSelection;
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please enter 'Rock', 'Paper', or 'Scissors'");
+    console.log(playRound(playerSelection, computerPlay()));
   }
 }
